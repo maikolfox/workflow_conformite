d3a79384e49778a80328e52dcbc49d619fde1276
bbgproject <maikol.ahoue@bridgebankgroup.com>
2020-09-16 18:21:53 +0000

    amelioration nav barre

--- src/components/assets/NavbarMain.jsx
+++ src/components/assets/NavbarMain.jsx
@@ -33,64 +33,69 @@ componentDidMount()
 }
   render() {
     return (
-//       <div>
-//       <Navbar fixed={"top"}  className='headerStyle' light expand="md" >
-//         <NavbarBrand href="/workflow-gestion-fnc/home">
-// <img src={window.location.origin+"/workflow-gestion-fnc/image/logoBridgeBank.png"} alt="BRIDGE BANK" style={{marginTop :'8px'}} height="50%" width="50%" />
-//         </NavbarBrand>
-//         <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-//         {/* <h6 id="navBarText">Workflow FNC</h6> */}
-//         <Collapse isOpen={!this.state.collapsed} navbar>
-//         <Nav className="ml-auto" navbar >
-//           <Col md={"auto"}>
-//           <UncontrolledDropdown nav inNavbar> 
-//             <DropdownToggle nav caret style={{color:"#cd511f", fontWeight:"bold",marginRight:"auto",marginTop:"5%" }}>
-//              <UserIcon/> {Auth.getDisplayName()}  
-//             </DropdownToggle> 
-//             <DropdownMenu right>
-//               <DropdownItem>
-//                 <NavItem>
-//                   <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} to="/Organisation">Espace Organisation</Link>
-//                 </NavItem>
-//               </DropdownItem>
-//               <DropdownItem divider />
-//               <DropdownItem>
-//                 <NavItem>
-//                   <Link style={{ color: 'black', textDecoration: 'none' }}  to="/DGRC">Espace DGRC</Link>
-//                 </NavItem>
-//               </DropdownItem>
-//               <DropdownItem divider />
-//               <DropdownItem>
-//                 <NavItem>
-//                   <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/ResponsableTraitement">Espace Responsable de traitement</Link>
-//                 </NavItem>
-//               </DropdownItem>
-//               <DropdownItem divider />
-//               <DropdownItem>
-//                 <NavItem>
-//                   <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/ActeurTraitant">Espace Acteur traitant </Link>
-//                 </NavItem>
-//               </DropdownItem>
-//               <DropdownItem divider />
-//               <DropdownItem>
-//               <NavItem>
-//                   <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/Home">Accueil </Link>
-//                 </NavItem>
-//               </DropdownItem>
-//               <DropdownItem divider />
-//               <DropdownItem>
-//               <NavItem>
-//                   <Link style={{ color: 'red', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/deconnexion">Déconnexion </Link>
-//                 </NavItem>
-//               </DropdownItem>
-//             </DropdownMenu>
-//           </UncontrolledDropdown>
-//           </Col>
-//         </Nav>
-//         </Collapse>
-//       </Navbar>
-//       </div>
-<header className="main-header">
+      <div>
+      <Navbar fixed={"top"}  className='headerStyle' light expand="md" >
+        <NavbarBrand href="/workflow-gestion-fnc/home">
+            <img src={window.location.origin+"/workflow-gestion-fnc/image/logoBridgeBank.png"} alt="BRIDGE BANK" style={{marginTop :'8px'}} height="50%" width="50%" />
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+        {/* <h6 id="navBarText">Workflow FNC</h6> */}
+        <Collapse isOpen={!this.state.collapsed} navbar>
+        <Nav className="ml-auto" navbar >
+          <Col md={"auto"}>
+          <UncontrolledDropdown nav inNavbar> 
+            <DropdownToggle nav caret style={{color:"#cd511f", fontWeight:"bold",marginRight:"auto",marginTop:"5%" }}>
+             <UserIcon/> {Auth.getDisplayName()}  
+            </DropdownToggle> 
+            <DropdownMenu right>
+              <DropdownItem>
+                <NavItem>
+                  <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} to="/Organisation">Espace Organisation</Link>
+                </NavItem>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                <NavItem>
+                  <Link style={{ color: 'black', textDecoration: 'none' }}  to="/DGRC">Espace DGRC</Link>
+                </NavItem>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                <NavItem>
+                  <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/ResponsableTraitement">Espace Responsable de traitement</Link>
+                </NavItem>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                <NavItem>
+                  <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/ActeurTraitant">Espace Acteur traitant </Link>
+                </NavItem>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+              <NavItem>
+                  <Link style={{ color: 'black', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/Home">Accueil </Link>
+                </NavItem>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+              <NavItem>
+                  <Link style={{ color: 'red', textDecoration: 'none' }} activeStyle={{ color: '#cd511f', textDecoration: 'none' }} className="navItemColor" to="/deconnexion">Déconnexion </Link>
+                </NavItem>
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+          </Col>
+        </Nav>
+        </Collapse>
+      </Navbar>
+      </div>
+
+    )
+  }
+}
+export default NavbarMain;
+{/* <header className="main-header">
                 <a href="#" className="logo">
                     <span className="logo-mini"><b>A</b>LT</span>
                     <span className="logo-lg"><b>Admin</b>LTE</span>
@@ -129,8 +134,4 @@ componentDidMount()
                         </ul>
                     </div>
                 </nav>
-            </header>
-    )
-  }
-}
-export default NavbarMain;
+            </header> */}
